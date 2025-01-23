1330a0a0a30cb6c5427ecb2409188a9837a2bac2
Srinivasa Reddy <reddy.grails@gmail.com>
2025-01-23 11:57:12 +0530

    commit
    First commit

---
+++ greet-service/src/test/java/comsprings/GreetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package comsprings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
